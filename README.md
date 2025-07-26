c07c0a0b0b156d86b1ea6aefe0e84da51e397b13
Ravi Kumar Pal <ravikumarpal906@gmail.com>
2025-07-27 05:27:38 +0530

    vizual code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phishing-classifier
+
+
+
+'''pip install -e .''' to run setup.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ravikumarpal",
+    author_email="ravikumarpal906@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
